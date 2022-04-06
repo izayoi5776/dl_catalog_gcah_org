3a7c068f1e96d1ae5b01b717d21c05c8bd88e59f
izayoi <izayoi5776@gmail.com>
2022-04-06 13:40:00 +0900

    add json, save file use id

--- app1.py
+++ app1.py
@@ -6,6 +6,7 @@ import sys
 import time
 from bs4 import BeautifulSoup
 from pathlib import Path
+import json
 
 # eg https://catalog.gcah.org/images/collections/show/6
 def get1url(url):
@@ -20,6 +21,7 @@ def get1url(url):
 # eg https://catalog.gcah.org/images/items/show/3337
 def getlvl2url(url):
   print("url=" + url, end="")
+  id = url.split("/")[-1]
   if(os.path.exists(os.path.basename(url) + ".html")):
     print("...SKIP")
   else:
@@ -29,10 +31,12 @@ def getlvl2url(url):
       soup = BeautifulSoup(html, "html5lib")
       # image
       for i in soup.select("div#item-images a"):
-        getFile(urllib.parse.urljoin(url, i["href"]))
+        getFile(urllib.parse.urljoin(url, i["href"]), id)
+        i["href"] = rewriteUrl(i["href"], id)
       # thumbnails
       for i in soup.select("div#item-images a img"):
-        getFile(urllib.parse.urljoin(url, i["src"]))
+        getFile(urllib.parse.urljoin(url, i["src"]), id)
+        i["src"] = rewriteUrl(i["src"], id)
       for i in soup.select("link"):
         try:
           getFile(urllib.parse.urljoin(url, i["href"]))
@@ -43,11 +47,35 @@ def getlvl2url(url):
           getFile(urllib.parse.urljoin(url, i["src"]))
         except:
           continue
-      
-      rewriteHtml(url, soup)
+
+      buildJson(soup, id)
+      rewriteHtml(soup, id)
+
+# 保存 json 文件
+def buildJson(soup, id):
+  ret = {}
+  ret["id"] = id
+  for i in soup.select("h3"):
+    ret[i.text] = i.parent.div.text
+  print(ret)
+  path = "json/" + id + ".json"
+  Path(os.path.dirname(path)).mkdir(parents=True, exist_ok=True)
+  with open(path, "w", encoding="utf-8") as f:
+    f.write(json.dumps(ret, indent=2, ensure_ascii=False))
+  return ret
+
+# 把 url 重写成 id 
+def rewriteUrl(url, id):
+  newurl = url
+  path = urllib.parse.urlparse(url).path
+  ext = path.split(".")[-1]
+  if path.startswith("/"):
+    path = path[1:]
+  newurl = os.path.dirname(path) + "/" + id + "." + ext
+  return newurl
 
 # 重写html
-def rewriteHtml(url, soup):
+def rewriteHtml(soup, id):
     for i in soup.select("a"):
       i["href"] = url2relative(i["href"])
     for i in soup.select("img"):
@@ -64,7 +92,7 @@ def rewriteHtml(url, soup):
     for i in soup.select("li#next-item a"):
       i["href"] = os.path.basename(i["href"]) + ".html"
     
-    writeHtmlFile(url, soup)
+    writeHtmlFile(soup, id)
 
 # URL改写成相对路径  
 def url2relative(url):
@@ -78,10 +106,15 @@ def url2relative(url):
   return path
 
 # eg https://catalog.gcah.org/images/files/original/b5d1859bda32cee341ef836646f476bc.jpg
-def getFile(url):
+#    id=3337
+# 保存 url 对应文件，带目录结构，按照 id 改名
+def getFile(url, id=None):
   path = urllib.parse.urlparse(url).path
+  ext = path.split(".")[-1]
   if path.startswith("/"):
     path = path[1:]
+  if id:
+    path = os.path.dirname(path) + "/" + id + "." + ext
   print(" path=" + path, end="")
   if(os.path.exists(path)):
     print("...SKIP")
@@ -89,20 +122,15 @@ def getFile(url):
     print("...GET")
     Path(os.path.dirname(path)).mkdir(parents=True, exist_ok=True)
     urllib.request.urlretrieve(url, path)
-  
-def writeHtmlFile(url, soup):
-  path = urllib.parse.urlparse(url).path
-  if path.startswith("/"):
-    path = path[1:]
-  if not path.endswith(".html"):
-    path = path + ".html"
-  path = os.path.basename(path)
+
+# 保存 html 文件
+def writeHtmlFile(soup, id):
+  path = id + ".html"
   print(" path=" + path, end="")
   if(os.path.exists(path)):
     print("...SKIP")
   else:
     print("...GET")
-    Path(os.path.dirname(path)).mkdir(parents=True, exist_ok=True)
     with open(path, "w", encoding="utf-8") as f:
       f.write(str(soup))
 
@@ -137,3 +165,4 @@ urls = (
 )
 for url in urls:
   get1url(url)
+#getlvl2url("https://catalog.gcah.org/images/items/show/3337")
